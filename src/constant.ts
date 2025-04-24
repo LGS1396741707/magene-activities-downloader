f3d8c1525cd57c703ff574e6af9fccb7d9a256c2
LGS <lgs1396741707@outlook.com>
2025-04-24 22:38:53 +0800

    Update constant.ts

--- src/constant.ts
+++ src/constant.ts
@@ -1,5 +1,5 @@
 export const LOGIN_PAGE = 'https://www.onelap.cn/login.html';
 export const DATA_PAGE = 'http://u.onelap.cn/analysis';
 
-export const USERNAME = '';
-export const PASSWORD = '';
+export const USERNAME = '18660220928';  
+export const PASSWORD = 'Ghy199209011314';
